6c78962e40744adacc3c43270ffe5473547b600d
Jack Davis <jbad90@gmail.com>
2020-01-22 10:27:42 +0000

    Implement basic web crawling

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,57 @@ extern crate scraper;
 
 use scraper::{Html, Selector};
 
-async fn get_body() -> Result<String, Box<dyn std::error::Error>> {
-    let body = reqwest::get("https://jbadavis.github.io")
-        .await?
-        .text()
-        .await?;
+async fn get_doc(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let doc = reqwest::get(url).await?.text().await?;
 
-    Ok(body)
+    Ok(doc)
 }
 
-fn get_links(body: &str) -> Vec<String> {
-    Html::parse_document(body)
+fn parse_links(doc: &str) -> Vec<String> {
+    Html::parse_document(doc)
         .select(&Selector::parse("a").unwrap())
-        .map(|elm| String::from(elm.value().attr("href").unwrap()))
+        .map(|elm| {
+            let href = elm.value().attr("href");
+
+            match href {
+                Some(url) => String::from(url),
+                None => String::new(),
+            }
+        })
+        .filter(|s| s.starts_with("http"))
         .collect()
+}
+
+async fn get_links(link: &str) -> Vec<String> {
+    match get_doc(link).await {
+        Ok(doc) => parse_links(&doc),
+        Err(_) => vec![],
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let body = get_body().await.unwrap();
+    let mut links: Vec<_> = vec![String::from("http://jbadavis.github.io")];
+    let mut link_store = links.clone();
 
-    let links = get_links(&body);
+    println!("Starting at {:?}\n", links[0]);
 
-    println!("{:?}", links);
+    for _i in 0..3 {
+        let mut new_links: Vec<_> = vec![];
+
+        for link in links.iter() {
+            println!("Searching {:?}", link);
+
+            let mut links_found: Vec<_> = get_links(&link).await;
+
+            println!("Found {:?} links\n", links_found.len());
+
+            new_links.append(&mut links_found);
+        }
+
+        links = new_links;
+        link_store.append(&mut links.clone());
+    }
+
+    println!("{:?}", link_store.len());
 }
